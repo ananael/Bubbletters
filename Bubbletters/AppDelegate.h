1dc032669e4fc3841c3d14653a505e814f1adf54
Michael Hoffman <mhoffman999@gmail.com>
2016-06-02 00:24:04 -0400

    New project. Added automated build increment and bones of main VC Storyboard.

---
+++ Bubbletters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bubbletters
+//
+//  Created by Michael Hoffman on 6/1/16.
+//  Copyright © 2016 Strong Atomic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bubbletters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bubbletters
+//
+//  Created by Michael Hoffman on 6/1/16.
+//  Copyright © 2016 Strong Atomic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
