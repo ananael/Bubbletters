009fe3036e5823d18ab33de59e406fd491025fb0
Michael Hoffman <mhoffman999@gmail.com>
2016-06-02 09:41:42 -0400

    Tweaked Storyboard for main VC and linked elements to main VC.

--- Bubbletters/ViewController.m
+++ Bubbletters/ViewController.m
@@ -10,6 +10,18 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *wordLabel;
+@property (weak, nonatomic) IBOutlet UIView *container01;
+@property (weak, nonatomic) IBOutlet UILabel *countLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIView *container02;
+@property (weak, nonatomic) IBOutlet UIView *container03;
+@property (weak, nonatomic) IBOutlet UIView *container04;
+@property (weak, nonatomic) IBOutlet UIView *container05;
+@property (weak, nonatomic) IBOutlet UIView *container06;
+@property (weak, nonatomic) IBOutlet UIView *container07;
+
+
 @end
 
 @implementation ViewController
@@ -17,11 +29,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
